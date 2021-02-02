2617222d50fc3a1214260b64f73a9b1fc33c9e89
Marek Bartoš <bartos.developer152@gmail.com>
2021-02-03 00:25:09 +0100

    Auth: load privileges and roles into authorizer

--- src/Auth/UserAuthorizerCreator.php
+++ src/Auth/UserAuthorizerCreator.php
@@ -2,14 +2,49 @@
 
 namespace OriCMF\UI\Auth;
 
+use OriCMF\Core\Role\RoleRepository;
 use Orisai\Auth\Authorization\PermissionAuthorizer;
 
 final class UserAuthorizerCreator
 {
 
+	private RoleRepository $roleRepository;
+
+	/** @var array<string> */
+	private array $privileges;
+
+	public function __construct(RoleRepository $roleRepository)
+	{
+		$this->roleRepository = $roleRepository;
+	}
+
 	public function create(): PermissionAuthorizer
 	{
-		return new PermissionAuthorizer();
+		$authorizer = new PermissionAuthorizer();
+
+		foreach ($this->privileges as $privilege) {
+			$authorizer->addPrivilege($privilege);
+		}
+
+		$roles = $this->roleRepository->findAll();
+
+		foreach ($roles as $role) {
+			$authorizer->addRole($role->name);
+
+			foreach ($role->privileges as $privilege) {
+				$authorizer->allow($role->name, $privilege);
+			}
+		}
+
+		return $authorizer;
+	}
+
+	/**
+	 * @param array<string> $privileges
+	 */
+	public function addPrivileges(array $privileges): void
+	{
+		$this->privileges = $privileges;
 	}
 
 }
