42f469dc07ee072e1771b0b304d5d808bb6f199c
Marek Bartoš <bartos.developer152@gmail.com>
2021-03-03 16:34:58 +0100

    UITemplateExtension: refactoring

--- src/Template/DI/UITemplateExtension.php
+++ src/Template/DI/UITemplateExtension.php
@@ -2,8 +2,16 @@
 
 namespace OriCMF\UI\Template\DI;
 
+use Nette\Application\UI\Template;
+use Nette\Bridges\ApplicationLatte\TemplateFactory;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Container;
 use Nette\DI\Definitions\ServiceDefinition;
+use OriCMF\UI\Admin\Auth\AdminFirewall;
+use OriCMF\UI\Control\Base\BaseControlTemplate;
+use OriCMF\UI\Front\Auth\FrontFirewall;
+use OriCMF\UI\Template\Locator\ControlTemplateLocator;
+use OriCMF\UI\Template\UITemplate;
 use function assert;
 
 final class UITemplateExtension extends CompilerExtension
@@ -15,35 +23,35 @@ final class UITemplateExtension extends CompilerExtension
 
 		$builder = $this->getContainerBuilder();
 
-		$templateFactoryDef = $builder->getDefinition('latte.templateFactory');
-		assert($templateFactoryDef instanceof ServiceDefinition);
+		$templateFactoryDefinition = $builder->getDefinition('latte.templateFactory');
+		assert($templateFactoryDefinition instanceof ServiceDefinition);
 
-		$adminFirewallDef = $builder->getDefinition('ori.ui.admin.auth.firewall');
-		$frontFirewallDef = $builder->getDefinition('ori.ui.front.auth.firewall');
-
-		$controlTemplateLocatorDef = $builder->getDefinition('ori.ui.template.locator.control');
-
-		$templateFactoryDef
-			->addSetup(
-				<<<'PHP'
-?->onCreate[] = function(\Nette\Application\UI\Template $template): void {
-	if ($template instanceof \OriCMF\UI\Template\UITemplate) {
-		$template->adminFirewall = ?;
-		$template->frontFirewall = ?;
-
-		if ($template instanceof \OriCMF\UI\Control\Base\BaseControlTemplate) {
-			$template->setTemplateLocator(?);
-		}
+		$templateFactoryDefinition->addSetup(
+			[self::class, 'prepareTemplate'],
+			[$templateFactoryDefinition, '@' . Container::class],
+		);
 	}
-};
-PHP,
-				[
-					'@self',
-					$adminFirewallDef,
-					$frontFirewallDef,
-					$controlTemplateLocatorDef,
-				],
-			);
+
+	public static function prepareTemplate(TemplateFactory $templateFactory, Container $container): void
+	{
+		$templateFactory->onCreate[] = static function (Template $template) use ($container): void {
+			if ($template instanceof UITemplate) {
+				$adminFirewall = $container->getByName('ori.ui.admin.auth.firewall');
+				assert($adminFirewall instanceof AdminFirewall);
+
+				$frontFirewall = $container->getByName('ori.ui.front.auth.firewall');
+				assert($frontFirewall instanceof FrontFirewall);
+
+				$template->adminFirewall = $adminFirewall;
+				$template->frontFirewall = $frontFirewall;
+
+				if ($template instanceof BaseControlTemplate) {
+					$controlTemplateLocator = $container->getByName('ori.ui.template.locator.control');
+					assert($controlTemplateLocator instanceof ControlTemplateLocator);
+					$template->setTemplateLocator($controlTemplateLocator);
+				}
+			}
+		};
 	}
 
 }
